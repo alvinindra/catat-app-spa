929a0eb17e3ac7b6b7459fb5228dc2f3f2379d6c
Alvin Indra <alvin@dicoding.com>
2023-02-03 14:11:40 +0700

    feat: add validation on input

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
+import { toast } from 'react-toastify'
 import PropTypes from 'prop-types'
 
 export default function NoteInput({ addNote }) {
+  const [isTitleValid, setIsTitleValid] = useState(false)
   const [noteData, setNoteData] = useState({ title: '', body: '' })
 
   const handleChangeTitle = (event) => {
+    if (noteData.title.length > 50) {
+      setIsTitleValid(true)
+    } else {
+      setIsTitleValid(false)
+    }
+
     setNoteData({
       ...noteData,
       title: event.target.value,
@@ -20,12 +28,27 @@ export default function NoteInput({ addNote }) {
 
   const handleSubmitNoteData = (event) => {
     event.preventDefault()
-    addNote(noteData)
+
+    if (noteData.title === '') {
+      toast.error('Judul tidak boleh kosong!')
+    } else if (noteData.body === '') {
+      toast.error('Detail catatan tidak boleh kosong!')
+    } else if (noteData.title !== '' && noteData.body !== '' && !isTitleValid) {
+      addNote(noteData)
+    }
   }
 
   return (
     <div className="px-4 lg:px-8 pb-8">
       <form className="flex flex-col" onSubmit={handleSubmitNoteData}>
+        <div className="flex flex-row mb-2">
+          <div className="lg:text-lg font-semibold ">Judul</div>
+          {isTitleValid && (
+            <div className="text-red-400 ml-2 my-auto text-sm">
+              *Tidak boleh lebih dari 50 karakter
+            </div>
+          )}
+        </div>
         <input
           type="text"
           placeholder="Masukan Judul"
@@ -33,10 +56,13 @@ export default function NoteInput({ addNote }) {
           value={noteData.title}
           onChange={handleChangeTitle}
         />
+        <div className="mt-4 flex flex-row mb-2">
+          <div className="lg:text-lg font-semibold ">Detail Catatan</div>
+        </div>
         <div
           type="text"
           data-placeholder="Masukan Detail Catatan"
-          className="mt-4 bg-white border min-h-[250px] placeholder:text-black focus:text-primary-dark text-sm rounded focus:ring-primary-blue focus:border-primary-blue block w-full px-4 py-4"
+          className="bg-white border min-h-[250px] placeholder:text-black focus:text-primary-dark text-sm rounded focus:ring-primary-blue focus:border-primary-blue block w-full px-4 py-4"
           value={noteData.body}
           onInput={handleChangeBody}
           contentEditable
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import NoteApp from '@/components/NoteApp'
 import '@/styles/global.scss'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <NoteApp />
+      <ToastContainer />
     </BrowserRouter>
   </React.StrictMode>
 )
